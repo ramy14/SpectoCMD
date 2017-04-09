fcfffb17eb249f393091ca143e0dcf18d4799b3c
ADMINRG-GBL1GR4\ahmed <ahmed.adelabbas@outlook.com>
2017-04-09 21:58:05 +0200

    adding the varaibles

--- Assets/SpectoCMD API/Scripts/SpectoCMDCore.cs
+++ Assets/SpectoCMD API/Scripts/SpectoCMDCore.cs
@@ -11,6 +11,12 @@ namespace SpectoCMD
         public string channelName;
         private string server = "irc.twitch.tv";
         private int port = 6667;
+        
+        public class MsgsEvent : UnityEngine.Events.UnityEvent<string> { } //this class inherits from unity events, to invoke recived msgs into a background callback
+
+        public MsgsEvent Streamedmessage = new MsgsEvent(); //creating an instance of the class 
+        private Queue<string> recivedCommands = new Queue<string>(); // the Queue contianing the commands
+        private Queue<string> commandMsgs = new Queue<string>(); // containing chat command msgs such as ping and bong and simmliar commadns
 
 
     }
